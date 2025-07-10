873a421b37986b5f7768ee8cf17fa9b28004edd0
Kilian Freitag <kilian.freitag@diefreitags.de>
2025-07-10 13:10:38 +0200

    update note datasets

--- main_stitch.py
+++ main_stitch.py
@@ -7,14 +7,11 @@ from src.util import load_tools, plot_tools
 from src.lpvds_class import lpvds_class
 
 
-def calculate_nodes(input_opt, plot_results=False):
+def calculate_nodes(input_opt):
     # data list with each entry containing x, x_dot, x_att, x_init
     data = load_tools.load_data_stitch(int(input_opt))
-
-    if plot_results:
-        fig, ax = plt.subplots(1, 1, figsize=(8, 5))
-
     node_centers = []
+    node_sigmas = []
     node_directions = []
     for i in range(len(data)):
         x, x_dot, x_att, x_init = data[i]
@@ -30,37 +27,34 @@ def calculate_nodes(input_opt, plot_results=False):
         for k in range(lpvds.damm.K):
             mean_xdot[k] = np.mean(x_dot[assignment_arr==k], axis=0)
 
-        for k in range(lpvds.damm.K):
-            sigma = lpvds.damm.Sigma[k, :, :]
-            mu = lpvds.damm.Mu[k, :]
-            if plot_results:
-                plot_tools.plot_2d_gaussian(mu, sigma, ax = ax)
-        
-        # plot mean xdot per cluster
-        if plot_results:
-            for k in range(lpvds.damm.K):
-                ax.arrow(centers[k, 0],
-                        centers[k, 1],
-                        mean_xdot[k, 0],
-                        mean_xdot[k, 1],
-                        head_width=0.5,
-                        head_length=0.5,
-                        fc='r',
-                        ec='r',
-                        zorder=10
-                        )
-
         node_centers.extend(centers.tolist())
         node_directions.extend(mean_xdot.tolist())
+        node_sigmas.extend(lpvds.damm.Sigma.tolist())
 
-        # plot original data if wanted
-        # ax.scatter(x[:, 0], x[:, 1], color='k', s=5, label='original data')
+    return np.array(node_centers), np.array(node_directions), np.array(node_sigmas)
 
-    if plot_results:
-        plt.tight_layout()
-        plt.show()  
 
-    return node_centers, node_directions
+def plot_nodes(node_centers, node_directions, node_sigmas):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+
+    for k in range(len(node_centers)):
+        mu = node_centers[k]
+        sigma = node_sigmas[k]
+        plot_tools.plot_2d_gaussian(mu, sigma, ax = ax)    
+
+        ax.arrow(node_centers[k, 0],
+                node_centers[k, 1],
+                node_directions[k, 0],
+                node_directions[k, 1],
+                head_width=0.5,
+                head_length=0.5,
+                fc='r',
+                ec='r',
+                zorder=10
+                )
+
+    plt.tight_layout()
+    plt.show()  
 
 
 def maybe_calculate_nodes(input_opt):
@@ -68,13 +62,16 @@ def maybe_calculate_nodes(input_opt):
     if os.path.exists(filename):
         print("Using cached nodes")
         with open(filename, 'r') as f:
-            node_centers, node_directions = json.load(f)
+            node_centers, node_directions, node_sigmas = json.load(f)
+            node_centers = np.array(node_centers)
+            node_directions = np.array(node_directions)
+            node_sigmas = np.array(node_sigmas)
     else:
         print("Calculating nodes")
-        node_centers, node_directions = calculate_nodes(input_opt)
+        node_centers, node_directions, node_sigmas = calculate_nodes(input_opt)
         with open(filename, 'w') as f:
-            json.dump([node_centers, node_directions], f)
-    return node_centers, node_directions
+            json.dump([node_centers.tolist(), node_directions.tolist(), node_sigmas.tolist()], f)
+    return node_centers, node_directions, node_sigmas
 
 def main():
     input_message = '''
@@ -85,7 +82,8 @@ def main():
     input_opt  = input(input_message)
 
     # 1) get GMM centers and directionality
-    node_centers, node_directions = maybe_calculate_nodes(input_opt)
+    node_centers, node_directions, node_sigmas = maybe_calculate_nodes(input_opt)
+    plot_nodes(node_centers, node_directions, node_sigmas)
 
     # 2) build graph
     print(node_centers)
